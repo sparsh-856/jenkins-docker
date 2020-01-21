17f83cb4ab780c5fbd36b00cdacb82d530aff481
Edwin Philip <edwin.philip@aptos.com>
2020-01-21 13:14:50 +0530

    improvement: added tailing-stream watcher in client
    added development supporting files such as .gitignore, .dockerignore, dockerfile, docker-compose etc

--- src/AlphaClient/index.js
+++ src/AlphaClient/index.js
@@ -1,3 +1,15 @@
 const 
     io = require ("socket.io-client"),
-    socket = io.connect("http://localhost:3000");
+    socket = io.connect("http://alphasolution_alphaserver_1:3000");
+
+const fs = require('fs');
+const TailingReadableStream = require('tailing-stream');
+
+const stream = TailingReadableStream.createReadStream("auth.log", {timeout: 0});
+
+stream.on('data', buffer => {
+    console.log(buffer.toString());
+});
+stream.on('close', () => {
+    console.log("close");
+});
