970e3d40ce4625c625b6a1c61ed09d51169093fa
Edwin Philip <edwin.philip@aptos.com>
2020-01-21 14:41:24 +0530

    feat: completed implementation of basic requirement

--- src/AlphaClient/index.js
+++ src/AlphaClient/index.js
@@ -1,15 +1,35 @@
+var os = require("os");
+var hostname = os.hostname();
+require('dotenv').config()
+
+
+var AlphaServerHostName;
+if(process.env.ALPHASERVER_PROTOCOL && process.env.ALPHASERVER_HOSTNAME && process.env.ALPHASERVER_PORT){
+    AlphaServerHostName = process.env.ALPHASERVER_PROTOCOL+"://"+process.env.ALPHASERVER_HOSTNAME+":"+process.env.ALPHASERVER_PORT;
+}else{
+    AlphaServerHostName = "http://localhost:3000"
+}
+
 const 
     io = require ("socket.io-client"),
-    socket = io.connect("http://alphasolution_alphaserver_1:3000");
+    socket = io.connect( AlphaServerHostName );
 
-const fs = require('fs');
-const TailingReadableStream = require('tailing-stream');
+socket.on("connect", () => {
+    socket.emit('client connected', hostname);
+    const fs = require('fs');
+    const TailingReadableStream = require('tailing-stream');
+    let numberOfAttpemts = 0;
+    const stream = TailingReadableStream.createReadStream("auth.log", {timeout: 0});
 
-const stream = TailingReadableStream.createReadStream("auth.log", {timeout: 0});
+    stream.on('data', buffer => {
+        file_data = buffer.toString();
+        if (file_data.includes('Failed password')){
+            numberOfAttpemts = numberOfAttpemts + 1;
+        }
+        socket.emit('attempts',numberOfAttpemts);
 
-stream.on('data', buffer => {
-    console.log(buffer.toString());
-});
-stream.on('close', () => {
-    console.log("close");
+    });
+    stream.on('close', () => {
+        console.log("close");
+    });
 });
--- src/AlphaServer/index.js
+++ src/AlphaServer/index.js
@@ -5,16 +5,23 @@ const
 
 const SERVER_PORT = 3000;
 
-let alphaClients = new Set();
+let alphaClients = new Map();
 
 
 function onNewWebsocketConnection(socket) {
-    console.info(`Socket ${socket.id} has connected.`);
-    alphaClients.add(socket.id);
+    let host_name;
+    socket.on('client connected', function(hostname){
+      alphaClients.set(hostname,0);
+      host_name = hostname;
+    });
+
+    socket.on('attempts',function(numberOfAttpemts){
+      alphaClients.set(host_name,numberOfAttpemts)
+      console.log('* node '+ host_name +' had '+numberOfAttpemts+ ' attempt');
+    });
 
     socket.on("disconnect", () => {
       alphaClients.delete(socket.id);
-        console.info(`Socket ${socket.id} has disconnected.`);
     });
 
   }
@@ -30,7 +37,10 @@ function onNewWebsocketConnection(socket) {
       // will fire for every new websocket connection
       io.on("connection", onNewWebsocketConnection);
 
-      server.listen(SERVER_PORT, () => console.info(`Listening on port ${SERVER_PORT}.`));
+      server.listen(SERVER_PORT, () => {
+        console.info(`Listening on port ${SERVER_PORT}.`)
+        console.info('Metrics for ssh log-in attempts')
+      });
 
   }
   
